81559c02d604b167f776a315b69fbe450d474632
AmirmohammadKazemi <amirmohammadkazemi2002@gmail.com>
2022-12-16 23:31:06 +0330

    basic functions

--- compiler.cpp
+++ compiler.cpp
@@ -1,13 +1,58 @@
 //libraries
 #include <iostream>
+#include <string>
 
 //namespaces
 using namespace std;
 
+//functions
+
+//print welcome message to user
+void startMessage() {
+	cout<<"\t...Welcom to MyLang language compiler...\n";
+}
+
+//isKey function for check word is key word or not
+int isKey(string word) {
+	return 0;
+}
+
+//isID function for check word is ID or not
+int isID(string word) {
+	return 0;
+}
+
+//isInt function for check word is integer Number or not
+int isInt(string word) {
+	return 0;
+}
+
+//isFloat function for check word is float number or not
+int isFloat(string word) {
+	return 0;
+}
+
+//isOperator function for check word is operator or not
+int isOperator(string word) {
+	return 0;
+}
+
+//isSymbol function for check word is symbol or not
+int isSymbol(string word) {
+	return 0;
+}
+
+//getToken get word and description and generate token for syntax analysis
+string genToken(string word, string desc) {
+	return "Token";
+}
+
+
 //start function
 int main() {
 
-    cout<<"Hello guys...!!!"<<endl;
+    startMessage(); //show start message
+	cout<<"Enter source file name:> ";
 
     return 0;
 }
